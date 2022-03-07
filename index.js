153b3074d0596a017ed53878a6fcf6ca79049319
ayush1852017 <46336517+ayush1852017@users.noreply.github.com>
2022-03-07 12:45:03 +0530

    test

---
+++ index.js
@@ -0,0 +1,38 @@
+let express = require("express");
+let app = express();
+
+app.use(logger);
+
+app.get("/books", (req, res) => {
+  res.send("This is books");
+});
+
+app.get("/libraries", checkPermission("librarian"), (req, res) => {
+  res.send("You are Welcome Librarian");
+});
+
+app.get("/authors", checkPermission("author"), (req, res) => {
+  res.send("You are welcome Author");
+});
+
+function checkPermission(role) {
+  return function (req, res, next) {
+    if (req.user.role == role) {
+      next();
+    } else {
+      res.send("Not authorized");
+    }
+  };
+}
+
+function logger(req, res, next) {
+  if (req.path == "/libraries" || req.path == "/authors" || req.path == "/books") {
+    next();
+  } else {
+    res.send("Not authorized");
+  }
+}
+
+app.listen(4000, () => {
+  console.log("This is port 4000");
+});
